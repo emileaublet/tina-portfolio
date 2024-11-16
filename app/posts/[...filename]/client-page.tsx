c1f292c7c3696f069f6dc48758d5f9acb0d935f8
Émile <emileaublet@gmail.com>
2024-11-16 09:02:26 -0500

    init

---
+++ app/posts/[...filename]/client-page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import type { PostQuery } from "@/tina/__generated__/types";
+import { useEffect, useRef } from "react";
+import { useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+
+interface ClientPageProps {
+  query: string;
+  variables: {
+    relativePath: string;
+  };
+  data: PostQuery;
+}
+
+export default function Post(props: ClientPageProps) {
+  // data passes though in production mode and data is updated to the sidebar data in edit-mode
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+  return (
+    <>
+      <TinaMarkdown content={data.post.body} />
+    </>
+  );
+}
