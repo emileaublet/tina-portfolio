a6f814e4d4e3628ea450e4f852b237c36cbca53e
Émile <emileaublet@gmail.com>
2024-11-22 06:30:12 -0500

    project

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,13 @@
 const nextConfig = {
   rewrites: async () => [
     {
-      source: '/admin',
-      destination: '/admin/index.html',
+      source: "/admin",
+      destination: "/admin/index.html",
     },
   ],
-}
+  images: {
+    domains: ["assets.tina.io"],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
